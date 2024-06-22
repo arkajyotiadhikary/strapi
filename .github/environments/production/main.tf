a1981a781ee88699d0153a9af6ea2f1c01f01fb1
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-06-22 11:10:59 +0530

    ✨ feat: Update user_data.sh script to setup and configure Strapi project
    🛠️ Install necessary packages (git, nodejs, npm)
    🔧 Configure Git with user credentials
    🐙 Clone the Strapi project repository and checkout the arka-prod branch
    🔒 Set appropriate permissions for the cloned repository
    📄 Create and configure the .env file with required environment variables
    📦 Install project dependencies and build the Strapi project
    🚀 Start Strapi using pm2 and configure pm2 to start on system boot

--- .github/environments/production/main.tf
+++ .github/environments/production/main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "strapi_sg" {
 
 resource "aws_instance" "ar_strapi_instance" {
   ami           = "ami-0f58b397bc5c1f2e8"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   subnet_id     = data.aws_subnet.first.id
   vpc_security_group_ids = [aws_security_group.strapi_sg.id]
   key_name = "ps_pd_a"
--- .github/environments/production/user_data.sh
+++ .github/environments/production/user_data.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
+# Update and install necessary packages
 sudo apt-get update
 sudo apt-get install -y git nodejs npm
+
+# Install pm2 globally
 sudo npm install -g pm2
+
+# Navigate to the home directory
 cd /home/ubuntu
 
 # Configure Git
 git config --global user.name "arkajyotiadhikary"
 git config --global user.email "arkajyotiadhikary15@gmail.com"
 
-# Clone and setup the Strapi project without using PAT
+# Clone and setup the Strapi project
 git clone https://github.com/PearlThoughts-DevOps-Internship/strapi.git
 cd strapi
 git checkout arka-prod
@@ -17,7 +22,29 @@ git checkout arka-prod
 sudo chown -R ubuntu:ubuntu /home/ubuntu/strapi
 sudo chmod -R 755 /home/ubuntu/strapi
 
+# Create .env file with the required environment variables
+cat <<EOT > .env
+HOST=0.0.0.0
+PORT=1337
+APP_KEYS="toBeModified1,toBeModified2"
+API_TOKEN_SALT=tobemodified
+ADMIN_JWT_SECRET=tobemodified
+TRANSFER_TOKEN_SALT=tobemodified
+JWT_SECRET=tobemodified
+EOT
+
+# Install dependencies and build the project
 npm install
 npm run build
+
+# Start the Strapi application with pm2
 pm2 start npm --name "strapi" -- run start
+
+# Save the pm2 process list and have it resurrected on reboot
 pm2 save
+pm2 startup
+
+# Ensure the pm2 process manager starts on boot
+sudo env PATH=$PATH:/usr/bin pm2 startup systemd -u ubuntu --hp /home/ubuntu
+
+echo "Setup complete. Strapi should now be running and accessible at http://<your-ec2-instance-public-ip>:1337"

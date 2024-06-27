41fde3721fb5d8e6d9f606ecada051146015ed57
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-06-27 21:53:38 +0530

    out fx

--- .github/environments/production/outputs.tf
+++ .github/environments/production/outputs.tf
@@ -1,11 +1,11 @@
 output "instance_public_ip" {
-  value = module.ec2.instance_public_ip
+  value = aws_instance.arka_strapi.public_ip
 }
 
 output "instance_id" {
-  value = module.ec2.instance_id
+  value = aws_instance.arka_strapi.id
 }
 
 output "security_group_id" {
-  value = aws_security_group.strapi_sg.id
+  value = aws_instance.arka_strapi.vpc_security_group_ids[0]
 }

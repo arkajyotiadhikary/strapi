fbd3cf895518e728a4778a4ab0d747461f3647b3
Arka Jyoti Adhikary <66660068+arkajyotiadhikary@users.noreply.github.com>
2024-06-27 21:52:04 +0530

    dp rmv

--- .github/environments/production/main.tf
+++ .github/environments/production/main.tf
@@ -50,15 +50,3 @@ resource "null_resource" "example" {
     ]
   }
 }
-
-output "instance_public_ip" {
-  value = aws_instance.arka_strapi.public_ip
-}
-
-output "instance_id" {
-  value = aws_instance.arka_strapi.id
-}
-
-output "security_group_id" {
-  value = aws_instance.arka_strapi.vpc_security_group_ids[0]
-}

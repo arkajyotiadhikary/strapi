ff75ea545c5d443ad4d1aa3e1a9c829bbe929787
arkajyotiadhikaryexit <arkajyotiadhikary15@gmail.com>
2024-06-28 09:31:18 +0530

    some fixes

--- .github/environments/production/variables.tf
+++ .github/environments/production/variables.tf
@@ -55,3 +55,15 @@ variable "region" {
   description = "AWS region to deploy resources in"
   type        = string
 }
+
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true
+}
